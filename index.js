22d3f66666ec17b5d0c6f9001aece8e7a4c82ac6
Alex Whitman <alex@alexwhitman.com>
2011-11-18 12:17:44 +0000

    Simplify module creation

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ var GitHubApi = require("github").GitHubApi;
 
 var github = new GitHubApi(true);
 
-module.exports = function(bot) {
-
-	var module = new bot.Module();
+module.exports = function(bot, module) {
 
 	module.load = function() {
 		require('./lib/users.js')(bot, this, github);
 	};
-
-	return module;
 
 };
